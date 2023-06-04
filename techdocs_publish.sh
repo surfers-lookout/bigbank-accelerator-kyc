7516f6c167a601b304f05360abd39cb741092ed1
davlloyd <davidlloyd0@gmail.com>
2023-06-04 19:35:54 +1000

    config change

--- techdocs_publish.sh
+++ techdocs_publish.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # Script to generate and publish techdopcs from current directory to gcs
 
-APPNAME='KYC'
+APPNAME='kyc'
 NAMESPACE='default'
 
 # This is just an example of how to apply gcs credentials for the document publishing
-export GOOGLE_APPLICATION_CREDENTIALS=~/gcr.json
+export GOOGLE_APPLICATION_CREDENTIALS=~/credentials/gcr.json
 
 techdocs-cli generate --source-dir . 
 techdocs-cli publish --publisher-type googleGcs --storage-name techdocs-tap --entity $NAMESPACE/Component/$APPNAME
